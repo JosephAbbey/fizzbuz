450adaf6ba4c669e2885a8cfe9fa12422e16e325
JosephAbbey <66320873+JosephAbbey@users.noreply.github.com>
2021-01-26 09:02:46 +0000

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,10 @@
 var n = 100;
 
 for (var i of Array(n).keys()) {
-  i++
-  console.log(i % 3 ? i % 5 ? i : "buzz" : i % 5 ? "fizz" : "fizzbuz");
+  i++;
+  var o = "";
+  if (!(i % 3)) { o += "fizz"; }
+  if (!(i % 5)) { o += "buzz"; }
+  if (o == "") { o = i; }
+  console.log(o);
 }

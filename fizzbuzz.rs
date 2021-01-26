d0ab4497e8bf0fa597ed2d578f814c6ffbccc35e
JosephAbbey <66320873+JosephAbbey@users.noreply.github.com>
2021-01-26 09:45:22 +0000

    Update fizzbuzz.rs

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -2,16 +2,10 @@ fn main() {
   let n = 100;
 
   for i in 1..(n + 1) {
-    if i % 3 == 0 {
-      if i % 5 == 0 {
-        println!("fizzbuz");
-      } else {
-        println!("fizz");
-      }
-    } else if i % 5 == 0 {
-      println!("buz");
-    } else {
-      println!("{}", i);
-    }
+    let mut o = "".to_string();
+    if i % 3 == 0 { o += "fizz"; } 
+    if i % 5 == 0 { o += "buzz"; }
+    if o == "" { o = i.to_string(); }
+    println!("{}", o);
   }
 }

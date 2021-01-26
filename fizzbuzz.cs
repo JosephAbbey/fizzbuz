57a94b767b6ac1d41c389ab6bdc8dd448582060e
JosephAbbey <66320873+JosephAbbey@users.noreply.github.com>
2021-01-26 09:14:53 +0000

    Update fizzbuzz.cs

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,33 +1,15 @@
 using System;
 
-public static class Program
-{
+public static class Program {
   public static int n = 100;
 
-
-  public static void Main()
-  {
-    for (int i = 1; i <= n; i++)
-    {
-      if (i % 3 == 0)
-      {
-        if (i % 5 == 0)
-        {
-          Console.WriteLine("fizzbuz");
-        }
-        else
-        {
-          Console.WriteLine("fizz");
-        }
-      }
-      else if (i % 5 == 0)
-      {
-        Console.WriteLine("buz");
-      }
-      else
-      {
-        Console.WriteLine(i);
-      }
+  public static void Main() {
+    for (int i = 1; i <= n; i++) {
+      string o = "";
+      if (i % 3 == 0) { o += "fizz"; }
+      if (i % 5 == 0) { o += "buzz"; }
+      if (i % 3 == 0) { o = (string)i; }
+      Console.WriteLine(o);
     }
   }
 }
